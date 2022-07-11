5833a3c14eec4620904489242351350a4fcc5f3b
FaizanNabi-hub <73012846+FaizanNabi-hub@users.noreply.github.com>
2022-07-12 02:17:58 +0530

    Add files via upload

---
+++ MyCapLab2.py
@@ -0,0 +1,14 @@
+list1 = [12,-7,5,64,-14]
+answers = []
+for i in list1:
+    if i > 0:
+        answers.append(i)
+print("Output : ", answers)
+
+
+list2 = [12,14,-95,3]
+answers1 = []
+for x in list2 :
+    if x > 0:
+        answers1.append(x)
+print("Output : ", answers1)
